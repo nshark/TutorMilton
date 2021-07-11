2adf4483f9b5e9b2e7543a5c252ddaabf09e3e4a
Sebastian Park <SebPark03@gmail.com>
2021-07-11 00:51:09 -0400

    Initial commit.

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,9 +1,27 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+var db = admin.firestore
+
+exports.createUser = functions.auth.user().onCreate(async (user) => {
+    const newUser = {
+      email: user.email,
+      createDate: db.FieldValue.serverTimestamp(),
+      displayName: user.displayName,
+      freePeriods: [],
+      isTutor: false,
+      isTutee: false,
+      tutorAvailable: false,
+      subjectsCanTutor: [],
+      subjectsNeededTutor: [],
+      dorm: "",
+      requestPending: false,
+      cellPhone: "",
+    }
+  
+    const userRef = db().collection("users");
+  
+    await userRef.doc(String(user.uid)).set(newUser);
+  
+  })
