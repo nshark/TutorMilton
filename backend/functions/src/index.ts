85503eb0e68676affe0ec7104e45e8f463c06ed2
Sebastian Park <SebPark03@gmail.com>
2021-07-11 16:31:35 -0400

    Update index.ts

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,27 +1,33 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-admin.initializeApp()
-var db = admin.firestore
+admin.initializeApp();
+let db = admin.firestore;
 
 exports.createUser = functions.auth.user().onCreate(async (user) => {
+    let gradYear;
+    gradYear = user.email?.split("@")[0]
+        .substr(user.email?.split("@")[0].length - 2);
+    // if(!isNaN(gradYear)){
+
+    // }
     const newUser = {
-      email: user.email,
-      createDate: db.FieldValue.serverTimestamp(),
-      displayName: user.displayName,
-      freePeriods: [],
-      isTutor: false,
-      isTutee: false,
-      tutorAvailable: false,
-      subjectsCanTutor: [],
-      subjectsNeededTutor: [],
-      dorm: "",
-      requestPending: false,
-      cellPhone: "",
-    }
-  
+        email: user.email,
+        gradYear: gradYear,
+        createDate: db.FieldValue.serverTimestamp(),
+        displayName: user.displayName,
+        freePeriods: [],
+        isTutor: false,
+        isTutee: false,
+        tutorAvailable: false,
+        subjectsCanTutor: [],
+        subjectsNeededTutor: [],
+        dorm: "",
+        requestPending: false,
+        cellPhone: "",
+    };
+
     const userRef = db().collection("users");
-  
+
     await userRef.doc(String(user.uid)).set(newUser);
-  
-  })
+});
