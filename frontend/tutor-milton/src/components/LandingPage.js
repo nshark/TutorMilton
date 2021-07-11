b53cb8004d51d6d9e64200cf55e52918e3a32999
Sebastian Park <SebPark03@gmail.com>
2021-07-11 00:00:44 -0400

    Edit colors

--- frontend/tutor-milton/src/components/LandingPage.js
+++ frontend/tutor-milton/src/components/LandingPage.js
@@ -8,18 +8,6 @@ export default class LandingPage extends Component {
             <div className="App-bg">
                 <h1 className="main-title">Welcome.</h1>
                 <div className="welcome-subtitle">Take me to the tutoring portal</div>
-                {/* {<img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn Reac
-      </a>} */}
             </div>]
         );
     }
