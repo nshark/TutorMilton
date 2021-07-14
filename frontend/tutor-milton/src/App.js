f3647de5cd0bf1df18f79e624984d7561131c4d8
Gunman108 <41013296+Gunman108@users.noreply.github.com>
2021-07-13 21:54:04 -0400

    Most of the CSS

--- frontend/tutor-milton/src/App.js
+++ frontend/tutor-milton/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from './components/LandingPage';
+import TutorProf from './components/TutorProf'
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <TutorProf />
     </div>
   );
 }
+
 
 export default App;
