1c353a42efc4894e54913903d0aae28b382a9cc5
PeterP1212 <68553123+PeterP12YT@users.noreply.github.com>
2021-09-04 23:52:23 -0400

    t

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,77 +1,104 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as Datetime from 'react-datetime';
 
-admin.initializeApp();
-const db = admin.firestore;
+admin.initializeApp(functions.config().firebase);
 
-exports.createUser = functions.auth.user().onCreate(async (user) => {
-    const gradYear = user.email?.split('@')[0]
-        .substr(user.email?.split('@')[0].length - 2);
-    // if(!isNaN(gradYear)){
+const app=express()
+const main=express()
 
-    // }
-    const newUser = {
-        email: user.email,
-        gradYear: gradYear,
-        createDate: db.FieldValue.serverTimestamp(),
-        displayName: user.displayName,
-        freePeriods: [],
-        isTutor: false,
-        isTutee: false,
-        dorm: '',
-        cellPhone: '',
-    };
+main.use('/Myapi',app)
+main.use(bodyParser.json())
+main.use(bodyParser.urlencoded({extended:false}))
 
-    const userRef = db().collection('users');
+const db=admin.firestore()
+export const webApi=functions.https.onRequest(main)
 
-    await userRef.doc(String(user.uid)).set(newUser);
-});
+interface User
+{
+    userEmail:String,
+    gradYear:Number,
+    createDate:Date,
+    displayName:String,
+    calendarEvents:[String, Datetime, Datetime, Number, String],
+    isTutor:Boolean,
+    
 
-export const addTutor = functions.https.onRequest(
-    async (req: any, res: any) => {
-        const newTutor = {
-            available: true,
-            subjectsToTutor: [],
-        };
 
-        const userRef = db().collection('tutors');
+}
 
-        userRef.doc(String(req.body.uid)).set(newTutor).then(() => {
-            res.status(200).json({
-                result: 'success',
-                tuteeInfo: newTutor,
-            });
-        }
-        ).catch((error: any) => {
-            res.status(200).json({
-                result: 'error: could not set document',
-                error: error,
-            });
-        }
-        );
-    });
 
-export const addTutee = functions.https.onRequest(
-    async (req: any, res: any) => {
-        const newTutee = {
-            requestPending: false,
-            subjectsNeeded: [],
-        };
+// const db = admin.firestore;
 
-        const userRef = db().collection('tutees');
+// exports.createUser = functions.auth.user().onCreate(async (user) => {
+//     const gradYear = user.email?.split('@')[0]
+//         .substr(user.email?.split('@')[0].length - 2);
+//     // if(!isNaN(gradYear)){
 
-        userRef.doc(String(req.body.uid)).set(newTutee).then(() => {
-            res.status(200).json({
-                result: 'success',
-                tuteeInfo: newTutee,
-            });
-        }
-        ).catch((error: any) => {
-            res.status(200).json({
-                result: 'error: could not set document',
-                error: error,
-            });
-        }
-        );
-    });
+//     // }
+//     const newUser = {
+//         email: user.email,
+//         gradYear: gradYear,
+//         createDate: db.FieldValue.serverTimestamp(),
+//         displayName: user.displayName,
+//         freePeriods: [],
+//         isTutor: false,
+//         isTutee: false,
+//         dorm: '',
+//         cellPhone: '',
+//     };
+
+//     const userRef = db().collection('users');
+
+//     await userRef.doc(String(user.uid)).set(newUser);
+// });
+
+// export const addTutor = functions.https.onRequest(
+//     async (req: any, res: any) => {
+//         const newTutor = {
+//             available: true,
+//             subjectsToTutor: [],
+//         };
+
+//         const userRef = db().collection('tutors');
+
+//         userRef.doc(String(req.body.uid)).set(newTutor).then(() => {
+//             res.status(200).json({
+//                 result: 'success',
+//                 tuteeInfo: newTutor,
+//             });
+//         }
+//         ).catch((error: any) => {
+//             res.status(200).json({
+//                 result: 'error: could not set document',
+//                 error: error,
+//             });
+//         }
+//         );
+//     });
+
+// export const addTutee = functions.https.onRequest(
+//     async (req: any, res: any) => {
+//         const newTutee = {
+//             requestPending: false,
+//             subjectsNeeded: [],
+//         };
+
+//         const userRef = db().collection('tutees');
+
+//         userRef.doc(String(req.body.uid)).set(newTutee).then(() => {
+//             res.status(200).json({
+//                 result: 'success',
+//                 tuteeInfo: newTutee,
+//             });
+//         }
+//         ).catch((error: any) => {
+//             res.status(200).json({
+//                 result: 'error: could not set document',
+//                 error: error,
+//             });
+//         }
+//         );
+//     });
